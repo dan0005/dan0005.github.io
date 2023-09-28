140fe50d5e05f33b94d4eb727b624a75fbf9e7ea
Daniel Newton <dnewton@smxtech.com>
2023-09-28 08:05:10 -0500

    Dark/light toggle

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -1,24 +1,27 @@
-import React from 'react'
-import { useTheme } from '@skagami/gatsby-plugin-dark-mode'
+import React from 'react';
+import { useTheme } from '@skagami/gatsby-plugin-dark-mode';
+import { FiSun, FiMoon } from 'react-icons/fi'; // importing icons from react-icons
 
-const ThemeToggle = () => {
-  const [theme, toggleTheme] = useTheme()
+export default function ThemeToggle() {
+  const [theme, toggleTheme] = useTheme();
 
-  // Don't render anything at compile time. Deferring rendering until we
-  // know which theme to use on the client avoids incorrect initial
-  // state being displayed.
   if (theme === null) {
-    return null
+    return null;
   }
 
   return (
-    <label>
+    <label className='relative inline-flex items-center cursor-pointer'>
       <input
-        type="checkbox"
+        type='checkbox'
+        value=''
+        className='sr-only peer'
         onChange={(e) => toggleTheme(e.target.checked ? 'dark' : 'light')}
         checked={theme === 'dark'}
       />
-      Dark mode
+      <div className="w-11 h-6 bg-gray-200 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-sky-900 flex flex-row items-center justify-evenly">
+        <FiMoon className='primary-text' />
+        <FiSun className='primary-text' />
+      </div>
     </label>
-  )
+  );
 }
